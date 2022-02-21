9bd2aff2dc783f26ece3e1cefed84938cc978743
Viacheslav Diukov <slavad156@ukr.net>
2022-02-21 23:08:58 +0200

    start 11 home work

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# goit-js-hw-11
+Home Work 11
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
